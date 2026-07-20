bbba7f053a484e322c298773f2bb10fc2e9f7daa
Lukas <38840142+lukasstorck@users.noreply.github.com>
2026-07-21 00:03:11 +0200

    fix: add missing slash in api endpoint path

--- src/app.py
+++ src/app.py
@@ -53,7 +53,7 @@ async def lifespan(app: fastapi.FastAPI):
 app = fastapi.FastAPI(lifespan=lifespan)
 
 
-@app.get(CALENDAR_NAME)
+@app.get(f'/{CALENDAR_NAME}')
 async def calendar(token: str = fastapi.Query(...)):
   if token != API_TOKEN:
     raise fastapi.HTTPException(status_code=401, detail='Invalid token')
